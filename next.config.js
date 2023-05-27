348a1e5a264682cade0bda0672fb009a8be09b0f
Najmul <najmulhasankhokon111@gmail.com>
2023-05-27 21:11:04 +0600

    remotePatterns was added in images as a domain name

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    images: {
-      domains: ["cdn.sanity.io"],
+      // domains: ["cdn.sanity.io"],
       // we can add the domain name like this also
-      // remotePatterns: [
-      //    {
-      //      protocol: 'https',
-      //      hostname: 'cdn.sanity.io',
-      //      port: '',
-      //    },
-      //  ],
+      remotePatterns: [
+         {
+            protocol: "https",
+            hostname: "cdn.sanity.io",
+            port: "",
+         },
+      ],
    },
 };
 
